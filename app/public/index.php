22d17d04320a32156c573edfda504d57841bc22b
Andrew Howden <hello@andrewhowden.com>
2017-02-26 13:20:55 +0100

    feat (logging): Set up logging to defined handler

--- app/public/index.php
+++ app/public/index.php
@@ -7,8 +7,13 @@
  * @license   MIT
  */
 
+use Monolog\Logger;
+use Monolog\Handler\ErrorLogHandler;
+use \Monolog\Formatter\JsonFormatter;
+use \Monolog\Handler\StreamHandler;
+
 // Todo: Think about making this an application with a routing stack etc. Can then include health checks to ensure
-// all things are spected.
+// all things are inspected (2.1)
 //
 // Health checks would include
 // - Is satis.json there
@@ -23,17 +28,29 @@ const HTTP_STATUS_OK                    = 200;
 const HTTP_STATUS_BAD_REQUEST           = 400;
 const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
 
+// Set up logging
+// Todo: One this is an application, move this into a more general bootstrap. (2.0)
+require_once DIR_APP_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$oLog          = new Logger('*');
+$oLogFormatter = new JsonFormatter();
+$oLogHandler   = new ErrorLogHandler();
+
+$oLogHandler->setFormatter($oLogFormatter);
+$oLog->pushHandler($oLogHandler);
+
 $command = DIR_BIN . DIRECTORY_SEPARATOR . 'satis';
+exec($command, $output, $exitCode);
 
-exec($command, $output, $returnVar);
-
-switch ($returnVar) {
+switch ($exitCode) {
     case 0:
         http_response_code(HTTP_STATUS_OK);
+        $oLog->addInfo('Satis successfully rebuilt.');
         echo "OK";
         break;
     default:
         http_response_code(HTTP_STATUS_INTERNAL_SERVER_ERROR);
         echo "NOT OK";
+        $oLog->addInfo(sprintf('Satis rebuild failed. Exit code "%s", output "%s"', $exitCode, $output));
         break;
 }
